b338ce00c35b0a8e67d77c3a21b3dec2dd4deb93
miku-wwl <38804125+miku-wwl@users.noreply.github.com>
2021-02-20 17:20:06 +1100

    2021/02/20

--- code/src/JavaVMStackSOF.java
+++ code/src/JavaVMStackSOF.java
@@ -8,11 +8,33 @@ public class JavaVMStackSOF {
     }
 
     public static void main(String[] args) throws Throwable {
-        JavaVMStackSOF oom = new JavaVMStackSOF();
-        try {
-            oom.stackLeak();
-        } catch (Throwable e) {
-            System.out.println("stack length:" + oom.stackLength);
-            throw e;
-        }
+        // 基本数据类型之间的 ==  是比较值，引用数据类型 == 比较的是地址值
+        // 1：在Java Heap中创建对象 2：在字符串常量池中添加 zhangsan
+        String a = new String("zhangsan");
+
+        // 调用 intern 方法，因上一步中已经将zhangsan存入常量池中，这里直接返回常量池 zhangsan 的引用地址
+        String b = a.intern();
+        // a 的地址在Java Heap中 ， b的地址在 常量池中 ，所以结果是flase
+
+        System.out.println(b);
+
+        System.out.println(b == a);
+
+
+
+        // 因为常量池中已经包含zhangsan，所以直接返回
+        String c = "zhangsan";
+        // b c 的地址一致，所以是true
+        System.out.println(b == c);
+
+
+        String str1 = new String("计算机")+"软件";
+
+
+
+        System.out.println(str1.intern() == str1);
+        String str2 = new StringBuilder("ja").append("va").toString();
+        System.out.println(str2.intern() == str2);
     }
+}
+
